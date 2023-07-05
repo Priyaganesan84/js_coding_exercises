411f23548461eb312fd76495100f1206d0029d51
Priyaganesan84 <136799329+Priyaganesan84@users.noreply.github.com>
2023-07-05 10:54:02 +0100

    Exercise2 Implemtation done

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -8,20 +8,47 @@ export function getFillings(sandwich) {
 export function isFromManchester(person) {
 	if (person === undefined) throw new Error('person is required');
 	// Your code here!
-	
+	const city = person.city;
+	if(city == "Manchester")
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 export function getBusNumbers(people) {
 	if (people === undefined) throw new Error('people is required');
 	// Your code here!
+	return Math.ceil(people/40);
 }
 
 export function countSheep(arr) {
 	if (arr === undefined) throw new Error('arr is required');
 	// Your code here!
+	/* filter method used to create new array contains retun the count based upon animal value sheep */
+    return arr.filter(animal => animal === 'sheep').length; 
 }
 
 export function hasMPostCode(person) {
 	if (person === undefined) throw new Error('person is required');
 	// Your code here!
+	let personMCode= person.address.postCode;
+	let personCity = person.address.city;
+	console.log(person.address.postCode);
+	if(personMCode.startsWith('M') && personCity != 'Manchester')
+	{
+		  return false;
+	}
+	else if(personMCode.startsWith('M'))
+	{
+		 return true;
+	}
+
+	else if(!personMCode.startsWith('M'))
+	{
+		return false;
+	}
 }
